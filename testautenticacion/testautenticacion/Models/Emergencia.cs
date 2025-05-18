6f4e825df4e707cdf15f3c71749d2224f371a476
Jesus Alejandro Estrada Gonzalez <estrada.gonzalez.jesusalejandro@gmail.com>
2025-05-18 12:46:50 -0600

    se creo la clase modelo Emergencia

---
+++ testautenticacion/testautenticacion/Models/Emergencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testautenticacion.Models
+{
+    public class Emergencia
+    {
+        public int Id { get; set; }
+        public string NombreMonitor { get; set; }
+        public string Edificio { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Comentarios { get; set; }
+    }
+}
