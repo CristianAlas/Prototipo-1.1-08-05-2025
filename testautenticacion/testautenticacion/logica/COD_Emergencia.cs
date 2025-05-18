31d0765b416d696ffa75218ed3ce43999c45629d
Jesus Alejandro Estrada Gonzalez <estrada.gonzalez.jesusalejandro@gmail.com>
2025-05-18 12:55:36 -0600

    se creo clase COD_Emergencia para la logica al momento de guardar un monitoreo de emergencia

---
+++ testautenticacion/testautenticacion/logica/COD_Emergencia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace testautenticacion.logica
+{
+    public class COD_Emergencia
+    {
+        public void GuardarEmergencia(string nombreMonitor, string edificio, DateTime fechaHora, string comentarios)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(edificio))
+                    throw new Exception("El edificio no puede ser nulo o vacío.");
+
+                if (comentarios == null)
+                    comentarios = "Sin comentarios"; // Asignar valor por defecto
+
+                string connectionString = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+
+                using (SqlConnection conexion = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Emergencias (NombreMonitor, Edificio, FechaHora, Comentarios) " +
+                                   "VALUES (@NombreMonitor, @Edificio, @FechaHora, @Comentarios)";
+
+                    using (SqlCommand comando = new SqlCommand(query, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@NombreMonitor", nombreMonitor);
+                        comando.Parameters.AddWithValue("@Edificio", edificio);
+                        comando.Parameters.AddWithValue("@FechaHora", fechaHora);
+                        comando.Parameters.AddWithValue("@Comentarios", comentarios);
+
+                        conexion.Open();
+                        comando.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al guardar emergencia: " + ex.Message);
+            }
+        }
+    }
+}
